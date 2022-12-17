57cdb93a8675833993a83002b226d8bdf5920311
zyllus17 <mrfhassan17@gmail.com>
2022-12-17 16:51:25 +0530

    Screen to display

---
+++ lib/screen1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Screen1 extends StatelessWidget {
+  const Screen1({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Screen1'),
+      ),
+      body: Center(
+        child: Text('Screen1'),
+      ),
+    );
+  }
+}
---
+++ lib/screen2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Screen2 extends StatelessWidget {
+  const Screen2({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Screen2'),
+      ),
+      body: Center(
+        child: Text('Screen2'),
+      ),
+    );
+  }
+}
